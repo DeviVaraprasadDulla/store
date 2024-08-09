fe5bf41a50712634bd96c22e2ec1d4942cfbbfbb
Devi VaraPrasad <132048779+DeviVaraprasadDulla@users.noreply.github.com>
2024-08-09 20:08:28 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,322 @@
+// const data =[
+//     {
+//         image:"./images/image1.jpeg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"./images/image2.jpeg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"./images/image3.jpg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"./images/image4.jpeg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"https://m.media-amazon.com/images/I/710-B3pVSLL._SY342_.jpg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"https://m.media-amazon.com/images/I/513O3TOMsOS._SY445_SX342_.jpg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"https://cdnp.targetpublications.org/media/catalog/product/cache/f63311a2a00c092df9b3c20ab1a8a20f/1/2/12473_2_1.jpg",
+//         title:'Sri Krishna, Bhagavad-gita 18.68',
+//         author:' Swami B.P.Puri',
+//         cost:'599'
+//     },
+//     {
+//         image:"https://m.media-amazon.com/images/I/51QxnVcjmvL._SY445_SX342_.jpg",
+//         title:"The Magic of the Lost Story Paperback",
+//         author:"navenn",
+//         cost:'200'
+
+//     }
+
+// ]
+// const computerScienceBooks = [
+//     {
+//         image: "https://m.media-amazon.com/images/I/51OReB5ou8L._SX379_BO1,204,203,200_.jpg",
+//         title: "Introduction to the Theory of Computation",
+//         author: "Michael Sipser",
+//         cost: "800"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+//         author: "Robert C. Martin",
+//         cost: "900"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51kQZG+Uc+L._SX258_BO1,204,203,200_.jpg",
+//         title: "The Pragmatic Programmer: Your Journey to Mastery",
+//         author: "Andrew Hunt, David Thomas",
+//         cost: "850"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51g+ZCwbEoL._SX379_BO1,204,203,200_.jpg",
+//         title: "Design Patterns: Elements of Reusable Object-Oriented Software",
+//         author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+//         cost: "1200"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/81kqrwS1nNL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Introduction to Algorithms",
+//         author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+//         cost: "1000"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/41GszJ2bwhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+//         title: "Artificial Intelligence: A Modern Approach",
+//         author: "Stuart Russell, Peter Norvig",
+//         cost: "1100"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/71u0Dp1qTNL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Code Complete: A Practical Handbook of Software Construction",
+//         author: "Steve McConnell",
+//         cost: "950"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51GbnP8z8jL._SX258_BO1,204,203,200_.jpg",
+//         title: "The Mythical Man-Month: Essays on Software Engineering",
+//         author: "Frederick P. Brooks Jr.",
+//         cost: "700"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/81OtwkiU6pL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "The Art of Computer Programming, Volumes 1-4",
+//         author: "Donald E. Knuth",
+//         cost: "5000"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/61WZ1HQN9zL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Computer Networking: A Top-Down Approach",
+//         author: "James Kurose, Keith Ross",
+//         cost: "850"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/81kDH-KqtQL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "You Don't Know JS: Scope & Closures",
+//         author: "Kyle Simpson",
+//         cost: "400"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51WffG-jfEL._SX331_BO1,204,203,200_.jpg",
+//         title: "Head First Design Patterns",
+//         author: "Eric Freeman, Elisabeth Robson",
+//         cost: "1100"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/71h6PpGaz9L._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "JavaScript: The Good Parts",
+//         author: "Douglas Crockford",
+//         cost: "500"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51y9ccE8+BL._SX379_BO1,204,203,200_.jpg",
+//         title: "Grokking Algorithms: An Illustrated Guide for Programmers and Other Curious People",
+//         author: "Aditya Bhargava",
+//         cost: "750"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/51lzNiD3udL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+//         title: "Cracking the Coding Interview: 189 Programming Questions and Solutions",
+//         author: "Gayle Laakmann McDowell",
+//         cost: "900"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/71tbalAHYCL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Structure and Interpretation of Computer Programs",
+//         author: "Harold Abelson, Gerald Jay Sussman",
+//         cost: "1000"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/515H9W4Q3aL._SX376_BO1,204,203,200_.jpg",
+//         title: "Refactoring: Improving the Design of Existing Code",
+//         author: "Martin Fowler",
+//         cost: "950"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/41hX8-5KpYL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+//         title: "The C Programming Language",
+//         author: "Brian W. Kernighan, Dennis M. Ritchie",
+//         cost: "650"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/81V4KyMm2VL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Python Crash Course",
+//         author: "Eric Matthes",
+//         cost: "800"
+//     },
+//     {
+//         image: "https://m.media-amazon.com/images/I/81BdMSuI5ZL._AC_UY327_FMwebp_QL65_.jpg",
+//         title: "Designing Data-Intensive Applications",
+//         author: "Martin Kleppmann",
+//         cost: "1200"
+//     }
+// ];
+const storyBooks = [
+    {
+        image: "https://m.media-amazon.com/images/I/81gepf1eMqL._SY466_.jpg",
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        cost: "400"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/71kxa1-0mfL._SY679_.jpg",
+        title: "1984",
+        author: "George Orwell",
+        cost: "350"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/71qovngeOcL._SY466_.jpg",
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        cost: "300"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/81YOuOGFCJL._SY679_.jpg",
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K. Rowling",
+        cost: "450"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/91HHqVTAJQL._AC_UL320_.jpg",
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        cost: "350"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/81OthjkJBuL._SY679_.jpg",
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        cost: "400"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/81t2CVWEsUL._AC_UL320_.jpg",
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        cost: "500"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/91b0C2YNSrL._AC_UL320_.jpg",
+        title: "The Chronicles of Narnia",
+        author: "C.S. Lewis",
+        cost: "600"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/415JmdZgIHL._SY445_SX342_.jpg",
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        cost: "800"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/51MufXcwD4L._SY445_SX342_.jpg",
+        title: "Moby Dick",
+        author: "Herman Melville",
+        cost: "450"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/31yiO3p7x6L._SY445_SX342_.jpg",
+                
+        title: "Jane Eyre",
+        author: "Charlotte Brontë",
+        cost: "350"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/51UBcTq-CGL._SY445_SX342_.jpg",
+        title: "Wuthering Heights",
+        author: "Emily Brontë",
+        cost: "300"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/91ORJa-xI9L._SY466_.jpg",
+        title: "Little Women",
+        author: "Louisa May Alcott",
+        cost: "350"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/71O3PTUA3vL._SY466_.jpg",
+        title: "Anna Karenina",
+        author: "Leo Tolstoy",
+        cost: "400"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/61V2NedeHQL._SX342_SY445_.jpghttps://m.media-amazon.com/images/I/61V2NedeHQL._SX342_SY445_.jpg",
+        title: "War and Peace",
+        author: "Leo Tolstoy",
+        cost: "600"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/71EwoNQypZL._AC_UL320_.jpg",
+        title: "Crime and Punishment",
+        author: "Fyodor Dostoevsky",
+        cost: "450"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/51bt7LtryoL._SY445_SX342_.jpg",
+        title: "The Kite Runner",
+        author: "Khaled Hosseini",
+        cost: "400"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/51APkyJzNlL._SY445_SX342_.jpg",
+        title: "The Book Thief",
+        author: "Markus Zusak",
+        cost: "450"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UL320_.jpg",
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        cost: "350"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/81gepf1eMqL._AC_UL320_.jpg",
+        title: "The Giver",
+        author: "Lois Lowry",
+        cost: "300"
+    }
+];
+
+
+let html = ''
+storyBooks.forEach(item =>
+{
+    html+=`
+ <div class="item">
+        <a href="https://en.wikipedia.org/wiki/Mahabharata" target="_blank">
+        <div class="image">
+            <img class = "book-image" src="${item.image}"alt="">
+        </div>
+        </a>
+        <div class="book-name">
+            ${item.title}
+        </div>
+        <div class="author-name">&gt;&nbsp;${item.author}</div>
+        <div class="price">&#8377;${item.cost}</div>
+        <div class="add-to-cart">
+            <button class="add-to-cart-btn">Buy Now</button>
+        </div>
+    </div>
+    `
+}
+);
+document.querySelector('.main').innerHTML =html;
